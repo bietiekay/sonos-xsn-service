7fb70638f16d45dad44d199ec805013761d84c50
Daniel Kirstenpfad <bietiekay@gmail.com>
2014-08-17 12:25:10 +0200

    added recent and upcoming feed

---
+++ sonos-xsn-service/sonos-xsn-service/xenim streaming service/model/xsn-recent-feed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sonosxsnservice
+{
+	public class xsn_recent_feed_item
+	{
+		public string website { get; set; }
+		public string begin { get; set; }
+		public string pubdate { get; set; }
+		public string author_name { get; set; }
+		public string link { get; set; }
+		public string unique_id { get; set; }
+		public string icon { get; set; }
+		public string end { get; set; }
+		public string description { get; set; }
+		public string title { get; set; }
+	}
+
+	public class xsn_recent_feed
+	{
+		public string description { get; set; }
+		public string feed_url { get; set; }
+		public string title { get; set; }
+		public List<xsn_recent_feed_item> items { get; set; }
+		public string language { get; set; }
+		public string link { get; set; }
+		public string id { get; set; }
+	}
+}
--- sonos-xsn-service/sonos-xsn-service/xenim streaming service/xsnDeserializer.cs
+++ sonos-xsn-service/sonos-xsn-service/xenim streaming service/xsnDeserializer.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace sonosxsnservice
 {
 	public static class xsnDeserializer
 	{
-		public static xsn_live_feed Deserialize(String Input)
+		public static xsn_live_feed UpdateLiveFeed(String LiveFeedURL)
+		{
+			WebClient client = new WebClient ();
+			client.Encoding = System.Text.Encoding.UTF8;
+
+			try
+			{
+				return DeserializeLiveFeed(client.DownloadString(LiveFeedURL));
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("UpdateLiveFeed Exception: "+e.Message);
+				return null;
+			}
+		}
+			
+		public static xsn_live_feed DeserializeLiveFeed(String Input)
 		{
 			return JsonConvert.DeserializeObject<xsn_live_feed> (Input);
 		}
