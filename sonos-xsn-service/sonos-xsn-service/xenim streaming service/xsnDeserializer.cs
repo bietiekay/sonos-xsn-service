06fadfac8afc60edd60daffce2ddb003b97fe963
Daniel Kirstenpfad <bietiekay@gmail.com>
2014-08-17 12:40:31 +0200

    updating works

--- sonos-xsn-service/sonos-xsn-service/xenim streaming service/xsnDeserializer.cs
+++ sonos-xsn-service/sonos-xsn-service/xenim streaming service/xsnDeserializer.cs
@@ -6,10 +6,14 @@ namespace sonosxsnservice
 {
 	public static class xsnDeserializer
 	{
+		#region Updaters
+
+		#region Live Feed
 		public static xsn_live_feed UpdateLiveFeed(String LiveFeedURL)
 		{
 			WebClient client = new WebClient ();
 			client.Encoding = System.Text.Encoding.UTF8;
+			client.Headers.Add ("user-agent", "sonos-xsn-service; https://github.com/bietiekay/sonos-xsn-service");
 
 			try
 			{
@@ -21,11 +25,64 @@ namespace sonosxsnservice
 				return null;
 			}
 		}
-			
+		#endregion
+
+		#region Upcoming Feed
+		public static xsn_upcoming_feed UpdateUpcomingFeed(String UpcomingFeedURL)
+		{
+			WebClient client = new WebClient ();
+			client.Encoding = System.Text.Encoding.UTF8;
+			client.Headers.Add ("user-agent", "sonos-xsn-service; https://github.com/bietiekay/sonos-xsn-service");
+
+			try
+			{
+				return DeserializeUpcomingFeed(client.DownloadString(UpcomingFeedURL));
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("UpdateUpcomingFeed Exception: "+e.Message);
+				return null;
+			}
+		}
+		#endregion
+
+		#region Recent Feed
+		public static xsn_recent_feed UpdateRecentFeed(String RecentFeedURL)
+		{
+			WebClient client = new WebClient ();
+			client.Encoding = System.Text.Encoding.UTF8;
+			client.Headers.Add ("user-agent", "sonos-xsn-service; https://github.com/bietiekay/sonos-xsn-service");
+
+			try
+			{
+				return DeserializeRecentFeed(client.DownloadString(RecentFeedURL));
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine("UpdateRecentFeed Exception: "+e.Message);
+				return null;
+			}
+		}
+		#endregion
+
+		#endregion	
+
+		#region Deserializers
 		public static xsn_live_feed DeserializeLiveFeed(String Input)
 		{
 			return JsonConvert.DeserializeObject<xsn_live_feed> (Input);
 		}
+
+		public static xsn_recent_feed DeserializeRecentFeed(String Input)
+		{
+			return JsonConvert.DeserializeObject<xsn_recent_feed> (Input);
+		}
+
+		public static xsn_upcoming_feed DeserializeUpcomingFeed(String Input)
+		{
+			return JsonConvert.DeserializeObject<xsn_upcoming_feed> (Input);
+		}
+		#endregion
 	}
 }
 
--- sonos-xsn-service/sonos-xsn-service/xsnservice.cs
+++ sonos-xsn-service/sonos-xsn-service/xsnservice.cs
@@ -8,11 +8,17 @@ namespace sonosxsnservice
 	{
 		private Configuration myConfiguration;
 		private bool shutdown;
+		private xsn_live_feed CurrentLiveFeed;
+		private xsn_recent_feed CurrentRecentFeed;
+		private xsn_upcoming_feed CurrentUpcomingFeed;
 
 		public xsnservice (Configuration incomingConfiguration)
 		{
 			myConfiguration = incomingConfiguration;
 			shutdown = false;
+			CurrentLiveFeed = null;
+			CurrentRecentFeed = null;
+			CurrentUpcomingFeed = null;
 		}
 
 		public void Shutdown()
@@ -27,11 +33,26 @@ namespace sonosxsnservice
 
 				try{
 					// update and act...
+
+					// Live Feed
+					CurrentLiveFeed = xsnDeserializer.UpdateLiveFeed(myConfiguration.GetLiveFeedURL());
+					if (CurrentLiveFeed != null)
+						Console.WriteLine("Updated xsn Live Feed - "+CurrentLiveFeed.items.Count+" streams online");
+
+					// Upcoming Feed
+					CurrentUpcomingFeed = xsnDeserializer.UpdateUpcomingFeed(myConfiguration.GetUpcomingFeedURL());
+					if (CurrentUpcomingFeed != null)
+						Console.WriteLine("Updated xsn Upcoming Feed - "+CurrentUpcomingFeed.items.Count+" streams upcoming");
+
+					// Recent Feed
+					CurrentRecentFeed = xsnDeserializer.UpdateRecentFeed(myConfiguration.GetRecentFeedURL());
+					if (CurrentRecentFeed != null)
+						Console.WriteLine("Updated xsn Recent Feed - "+CurrentRecentFeed.items.Count+" streams done");
 				}
 				catch(Exception e)
 				{
 					// pokemon handling, catching them all, because we want this to run "unlimitedly"
-					Console.WriteLine ("Pokemon: "+e.Message);
+					Console.WriteLine ("Exception: "+e.Message);
 				}
 				Thread.Sleep (myConfiguration.GetPollingInterval()*1000);
 			}
